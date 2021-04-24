f3cfecab957d73dafb2d1ee6625261b19e0fbbad
alexxxstep <astep2004@gmail.com>
2021-04-24 13:32:08 +0300

    Layout, Header, Footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Layout.module.css';
+import Footer from './Footer';
 import Header from './Header';
 
 export default function Layout({title, keywords, description, children}) {
@@ -15,6 +16,8 @@ export default function Layout({title, keywords, description, children}) {
       <Header />
 
       <div className={styles.container}>{children}</div>
+
+			<Footer />
     </div>
   );
 }
