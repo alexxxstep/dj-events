9fa165f2ca97c3beacf7e40d85d12cb2fcb2cc08
alexxxstep <astep2004@gmail.com>
2021-04-24 20:02:48 +0300

    add Showcase, 404 page and other

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
 import Head from 'next/head';
+import {useRouter} from 'next/router';
 import styles from '../styles/Layout.module.css';
 import Footer from './Footer';
 import Header from './Header';
+import Showcase from './Showcase';
 
 export default function Layout({title, keywords, description, children}) {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
@@ -14,10 +18,11 @@ export default function Layout({title, keywords, description, children}) {
       </Head>
 
       <Header />
+      {router.pathname === '/' && <Showcase />}
 
       <div className={styles.container}>{children}</div>
 
-			<Footer />
+      <Footer />
     </div>
   );
 }
